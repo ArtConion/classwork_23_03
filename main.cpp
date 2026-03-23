864a115d9cffbcd803c6384b845236ca280dc9ce
ArtConion <artiom0408@mail.ru>
2026-03-23 10:55:52 +0300

    tests mini-framework

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include "vector-top-it.hpp"
+
+
+bool testDefaultVector()
+{
+  using topit::Vector;
+  Vector< int > v;
+  return v.isEmpty();
+}
+
 int main()
 {
-  using namespace topit;
-  Vector< int > a;
+  using test_t = bool(*)();
+  test_t tests[] = {
+    testDefaultVector
+  };
+
+  size_t size = sizeof(tests) / sizeof(test_t);
+  std::cout << std::boolalpha;
+  for (size_t i = 0; i < size ; ++i)
+  {
+    std::cout << tests[i]() << ": " << i << '\n';
+  }
 }
